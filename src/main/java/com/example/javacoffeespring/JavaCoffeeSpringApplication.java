7962ae54b9234e7ecc698ac3d74b0f9882cb0d95
Sean Misra <seanmisra30@gmail.com>
2023-01-17 08:46:15 -0500

    first commit

---
+++ src/main/java/com/example/javacoffeespring/JavaCoffeeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacoffeespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoffeeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCoffeeSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javacoffeespring/JavaCoffeeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacoffeespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoffeeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
