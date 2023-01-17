d9785aaa77441270804247ef6f59f07e50293a3c
Sean Misra <seanmisra30@gmail.com>
2023-01-17 12:59:25 -0500

    JPA/MySQL integration

--- src/main/java/com/example/javacoffeespring/models/MenuItem.java
+++ src/main/java/com/example/javacoffeespring/models/MenuItem.java
@@ -1,12 +1,28 @@
 package com.example.javacoffeespring.models;
 
+import jakarta.persistence.*;
+
+import javax.annotation.processing.Generated;
+
+@Entity
+@Table
 public class MenuItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "item_name")
     private String name;
+    @Column(name = "item_desc")
     private String description;
+    @Column(name = "price")
     private Float price;
 
+    public MenuItem() {
+    }
+
     public MenuItem(String name, String description, Float price) {
+        super();
         this.name = name;
         this.description = description;
         this.price = price;
@@ -22,6 +38,10 @@ public class MenuItem {
 
     public Float getPrice() {
         return this.price;
+    }
+
+    public long getId() {
+        return this.id;
     }
 
 }
--- src/main/java/com/example/javacoffeespring/models/UserAccount.java
+++ src/main/java/com/example/javacoffeespring/models/UserAccount.java
@@ -1,13 +1,32 @@
 package com.example.javacoffeespring.models;
 
+import jakarta.persistence.*;
+
+@Table
+@Entity
 public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+
+    // TODO: Encrypt this value!
+    @Column(name = "password")
     private String password;
+    @Column(name = "email")
     private String email;
+    @Column(name = "loyalty_points")
     private Long loyaltyPoints = 10L; // new user starts with 10 points
 
+    public UserAccount() {
+    }
     public UserAccount(String firstName, String lastName, String password, String email) {
+        super();
         this.firstName = firstName;
         this.lastName = lastName;
         this.password = password;
